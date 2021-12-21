caac470780e677d1d7a70f5e08ecb9f1f0534e71
aydanuwa <95673841+aydanuwa@users.noreply.github.com>
2021-12-21 14:11:39 +0600

    Add files via upload

---
+++ PainterModified/PainterModified.java
@@ -0,0 +1,25 @@
+package com.example.javafx.PainterModified;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class PainterModified extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root =
+                FXMLLoader.load(getClass().getResource("PainterModified.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setTitle("Painter"); // displayed in window's title bar
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
